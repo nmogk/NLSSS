8875837bc73ed5dfd2fce05e90e5320a197d1bab
nmogk <4602943+nmogk@users.noreply.github.com>
2023-11-29 22:50:50 -0700

    Added algorithms

--- wisereplication.py
+++ wisereplication.py
@@ -9,12 +9,24 @@ import pickle
 
 search_lvl = 5
 threshold_distance_km = 100
+taxon_level = 'species' # species, genus, family
+env_type = None # None, terr, marine envtype
 
 rv = StrEnum('ResponseVocab', [('ID', 'oid'), ('NAME', 'nam'), ('MAX_MA', 'eag'), ('MIN_MA', 'lag'), ('PARENT', 'pid'), ('LEVEL', 'lvl'), ('LAT', 'lat'), ('LON', 'lng'), ('SPECIES', 'tna'), ('PRECISION', 'prc'), ('FAMILY', 'fml'), ('GENUS', 'gnl')])
 
 column_filename = 'column.pkl'
 database_filename = 'paleobiodb.sqlite'
 api_base = 'https://paleobiodb.org/data1.2/'
+
+# Download files for all species in the lower and upper intervals of a boundary
+# Delete all records identified less precicely than species level
+# Consider at the species level those identified to subspecies level
+# Delete all records located less precicely than interval
+# Accept local stratigraphic units where boundaries are close to global boundaries (2My for most, 1My for units 2 My and shorter, 0My for top 2)
+# Combine files
+# Delete species found in only one side of boundary
+# Delete species (sic, occurrences?) located more than 2 degrees away from any other specimen on the other side of the boundary
+# Count species
 
 def queryColumn():
     interval_request = 'intervals/list.json?scale=1&level={}'
@@ -84,7 +96,7 @@ with sqlite3.connect(database_filename) as conn:
     # Perform spatial queries using SpatiaLite functions
     cursor = conn.cursor()
     
-    occurrence_request = 'occs/list.json?interval_id={}&pres=regular&show=acconly,class,coords,loc&idreso=species'
+    occurrence_request = 'occs/list.json?interval_id={}&pres=regular&show=acconly,class,coords,loc&idreso=' + taxon_level
     check_table_query = 'SELECT 1 FROM sqlite_master WHERE type="table" AND name="{}"'
     create_table_query = 'CREATE TABLE {}(' + ', '.join((rv.ID, rv.LAT, rv.LON, rv.PRECISION, rv.SPECIES, rv.GENUS, rv.FAMILY)) +  ')'
     insert_query = 'INSERT INTO {} VALUES (?, ?, ?, ?, ?, ?, ?)'
@@ -113,9 +125,19 @@ with sqlite3.connect(database_filename) as conn:
         cursor.executemany(insert_query.format(tablename), (get_insert_values(occ) for occ in occs))
         conn.commit()
 
-# print('Processing boundaries...')
-# for below, above in tqdm(more_itertools.windowed(column, 2), total=114):
-#     pass
+print('Processing boundaries...')
+for below, above in tqdm(more_itertools.windowed(column, 2), total=114):
+    pass
+    # Algorithm:
+    # Count total unique species
+    # Count unique species which globally cross boundary
+    # Starting from bottom, proceding upwards:
+    #     Delete occurrences of species unique to lower unit
+    #     Delete occurrences in lower unit farther than threshold distance from occurrences of the same species in upper unit
+    #     Count remaining unique species
+    # Save results data
+    # {id: {bdry_name:, total_species:, ngsss:, nlsss:}}
+    # Export to csv
 
 # query = f"""
 #     SELECT *
