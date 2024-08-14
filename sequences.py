26730eeb8f1740ce8513a306b52357d54808ea87
MogkNathan <nwm@rincon.com>
2024-08-14 16:38:04 -0700

    Changed sequences to time binning

--- sequences.py
+++ sequences.py
@@ -10,10 +10,13 @@ column_filename = 'column.pkl'
 with open(column_filename, 'rb') as f:
     column = pickle.load(f)
 
-    for interval in column:
-        res = req.get(baseUrl+f'sections?interval_name={interval[rv.NAME]}')
+    for interval in range(0, 540, 6):
+        # res = req.get(baseUrl+f'sections?interval_name={interval[rv.NAME]}&environ_class=marine')
+        res = req.get(baseUrl+f'sections?age_bottom={interval}&age_top={interval+10}')
         try:
-            packages = len(res.json()['success']['data'])
-            print(f'{interval[rv.NAME]}, {packages}')
+            # Filter out 0 thickness packages
+            filtered = [x for x in res.json()['success']['data'] if x['max_thick'] != '0.00']
+            packages = len(filtered)
+            print(f'{interval}, {packages}')
         except:
-            print(f'{interval[rv.NAME]}, -')
+            print(f'{interval}, -')
