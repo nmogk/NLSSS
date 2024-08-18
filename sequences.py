f5541117f796942fca6bcde2ba8f5ec658867b5e
Nathan Mogk <4602943+nmogk@users.noreply.github.com>
2024-08-18 09:19:32 -0700

    Plotting improvements, query by age

--- sequences.py
+++ sequences.py
@@ -8,7 +8,10 @@ from tqdm import tqdm
 
 baseUrl = 'https://macrostrat.org/api/'
 
-data_filename = 'packages.pkl'
+data_filename = 'packages_all_Ages.pkl'
+use_stages = True
+do_smooth = True
+step = 6
 
 sloss_ms = np.array([462, 396, 324, 132, 60])
 peters_ms = np.array([252, 186])
@@ -18,15 +21,25 @@ try:
     with open(data_filename, 'rb') as f:
         x, y = pickle.load(f)
 
-except FileNotFoundError:
-    step = 6
-    x = np.arange(0, 540, step)
-    y = np.zeros_like(x)
+except:
 
+    if use_stages:
+        res = req.get(f'{baseUrl}defs/intervals?timescale_id=1')
+        data = res.json()['success']['data']
+        queries = [x['name'] for x in data]
+        x = [x['t_age'] for x in data]
+    else:
+        x = np.arange(0, 540, step)
+
+    y = np.zeros_like(x)
     with open(data_filename, 'wb') as f:
         for i, interval in enumerate(tqdm(x)):
             # res = req.get(baseUrl+f'sections?interval_name={interval[rv.NAME]}&environ_class=marine')
-            res = req.get(baseUrl+f'sections?age_top={interval}&age_bottom={interval+step}&environ_class=marine')
+            
+            if use_stages:
+                res = req.get(f'{baseUrl}sections?interval_name={queries[i]}')#&environ_class=non-marine')
+            else:
+                res = req.get(baseUrl+f'sections?age_top={interval}&age_bottom={interval+step}')#&environ_class=non-marine')
 
             # Filter out 0 thickness packages
             filtered = [x for x in res.json()['success']['data'] if x['max_thick'] != '0.00']
@@ -34,20 +47,33 @@ except FileNotFoundError:
             y[i] = packages
         pickle.dump((x,y), f)
 
-window = 5
-kernel = np.array([0.1, .2, 0.4,  .2 ,0.1])#np.ones(window)/window
-y = np.convolve(y, kernel, mode='same')
+if do_smooth:
+    window = 5
+    kernel = np.array([0.1, .2, 0.4,  .2 ,0.1])#np.ones(window)/window
+    y = np.convolve(y, kernel, mode='same')
 # print(x)
 # print(y)
 
-plt.title('Number of gap bound packages')
+plt.title(f'Number of marine gap bound packages{", smoothed" if do_smooth  else ""}')
+axes = plt.gca()
+axes.set_xlim([0, 540])
+axes.set_xlabel('Ma')
+axes.set_ylabel('Package count')
 for bdry in sloss_ms:
-    plt.axvline(bdry, color='gray', linestyle='--')
+    plt.axvline(bdry, color='gray', linestyle='--', label='Sloss (1963) boundaries')
 for bdry in peters_ms:
-    plt.axvline(bdry, color='blue', linestyle='--')
+    plt.axvline(bdry, color='blue', linestyle='--', label='Peters (2008) boundaries')
 for bdry in mogk_ms:
-    plt.axvline(bdry, color='gold', linestyle=':')
-plt.plot(x,y)
+    plt.axvline(bdry, color='gold', linestyle=':', label='Additional boundaries')
+plt.plot(x,y, label=f'Gap bound packages{", smoothed" if do_smooth  else ""}')
 plt.scatter(x, y)
-plt.gca().invert_xaxis()
+axes.invert_xaxis()
+
+handles, labels = plt.gca().get_legend_handles_labels()
+
+# labels will be the keys of the dict, handles will be values
+temp = {k:v for k,v in zip(labels, handles)}
+
+plt.legend(temp.values(), temp.keys(), loc='best', framealpha=1)
+
 plt.show()
